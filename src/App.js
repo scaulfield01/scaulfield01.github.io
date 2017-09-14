476e8b06d714e60d642b7ea838d19cae18ac4783
scaulfield01 <s.caulfield88@gmail.com>
2017-09-13 19:12:49 -0700

    placeholder

--- src/App.js
+++ src/App.js
@@ -8,52 +8,15 @@ import rightSymbol from './images/rightSymbol.png';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayPage: 'about', // splash, landing, portfolio, about, music, contact,
-      imageFlip: 'right'  //portfolio, about, music, contact,
-    };
-    this.onNavClick = this.onNavClick.bind(this); // change display when link clicked
-    this.onNavHover = this.onNavHover.bind(this);
-  }
+    constructor(props) {
+        super(props);        
+    }
 
-  onNavClick(e) {
-    e.preventDefault();
-    let targetDisplayPage = e.target.dataset.displaypage;
-    this.setState({ displayPage: targetDisplayPage });
-  }
-
-  onNavHover(e) {
-    e.preventDefault();
-    let hoverDisplay = e.target.dataset.displaypage;
-    if( hoverDisplay === 'about'|| hoverDisplay === 'portfolio' ) {
-      this.setState({imageFlip: 'left'});
-    } else {
-      this.setState({imageFlip: 'right'});
-    };
-  }
-
-  render() {
-    let symbolImage = this.state.imageFlip === 'left' ? leftSymbol : rightSymbol;
-    return (
-      <div className="App">
-        <div className="imageWrapper">
-          <img className="symbolImage" src={symbolImage}/>
-        </div>
-        <div className="pageWrapper">
-          <NavigationBar
-            onNavClick={this.onNavClick}
-            displayPage={this.state.displayPage}
-            onNavHover={this.onNavHover}
-          />
-          <div className="itemWrap">
-            <MainContainer displayPage={this.state.displayPage} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            
+        );
+    }
 }
 
 export default App;
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,54 +1,25 @@
-import React, { Component} from 'react';
-import {Description, ComputerImage, ErrorImage, Lightswitch, AnimatedVectors, IconWrapper, MusicInfo} from './ContainerElements';
+import React, { Component } from 'react';
 
 class MainContainer extends Component {
 
-  // static propTypes = {
-  //   displayPage: React.PropTypes.string.isRequired
-  // }
-
-  // static defaultProps = {
-  //   displayPage: 'landing'
-  // }
-
-  render() {
-
-    switch(this.props.displayPage) {
-      case 'splash':
-        return (
-          <Lightswitch />
-        );
-      case 'landing':
-        return (
-          <AnimatedVectors />
-        );
-      case 'about':
-        return (
-          <Description />
-        );
-      case 'portfolio':
-        return (
-            <ComputerImage />
-        );
-      case 'music':
-        return (
-            <MusicInfo/>
-        );
-      case 'contact':
-        return (
-            <IconWrapper/>
-        );
-      case 'error':
-        return (
-          <div>
-            <ErrorImage />
-            <Description />
-          </div>
-        );
-      default:
-        return <h1>hi</h1>;
+    constructor(props) {
+        super(props);        
     }
-  }
+
+    render() {
+    	let layoutArray = [NavBar, ProfileImage, Portfolio, Contact];
+        return (
+            <div className="MainContainer">
+                {
+                    layoutArray.map((section) => {
+                        <div class="container">
+
+                        </div>
+                    })
+                }
+            </div>
+        );
+    }
 }
 
-export default MainContainer;
+export default App;
